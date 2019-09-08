cc56821f2cfec99221d76158aba48268ab749838
Sophia <sophiayun@yahoo.com>
2019-09-07 19:33:32 -0700

    React Component State - Sophia Yun

---
+++ src/components/format_time.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+
+class FormatTime extends Component{
+    constructor(props){
+        super(props);
+
+        this.timerStyle={
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '383px'
+        }
+        
+
+    }
+
+    leadingZero(number){
+        if(number < 10){
+            return '0'+number;
+
+        }
+        else {
+            return number;
+        }
+    }
+
+    trailingZero(number){
+        if(number < 10){
+            return number+'0';
+        }
+        else{
+            return number;
+        }
+    }
+
+    millisecondsToTime(){
+        const {elapsed} = this.props;
+
+        const sec = elapsed/1000;
+
+        const min = sec/60;
+
+        return {
+            hour: Math.floor(min/60),
+            min: Math.floor(min%60),
+            sec: Math.floor(sec%60),
+            ms: Math.floor(elapsed%100),
+        }
+
+    }
+
+   
+
+    render(){
+        const {hour, min, sec, ms} = this.millisecondsToTime();
+
+        return(
+            <div style={this.timerStyle}>{this.leadingZero(hour)}:{this.leadingZero(min)}:{this.leadingZero(sec)}.{this.trailingZero(ms)}</div>
+
+        )
+    }
+
+
+}
+
+
+export default FormatTime;
